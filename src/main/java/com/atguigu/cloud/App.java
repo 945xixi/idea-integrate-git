e7c51458e5cb5fd0018dc743b3cd09a20a70ae80
945xixi <15800020869@163.com>
2025-10-17 15:10:05 +0800

    hot-fix second commit

--- src/main/java/com/atguigu/cloud/App.java
+++ src/main/java/com/atguigu/cloud/App.java
@@ -11,6 +11,6 @@ public class App
         System.out.println( "Hello World  1111111" );
         System.out.println( "Hello World  2222222" );
         System.out.println( "Hello World  3333333" );
-        System.out.println( "Hello World  hot-fix" );
+        System.out.println( "Hello World  hot-fix retry" );
     }
 }
